eca0a63d71923df8efb819712d0521cda80b7272
xuliji <2384465976@qq.com>
2022-10-31 20:49:36 +0800

    update

--- src/go_code/chapter03/stringdemo05/main.go
+++ src/go_code/chapter03/stringdemo05/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 // 字符类型使用
 // golang没有专门的字符类型,可以用byte保存
@@ -14,6 +17,14 @@ func main() {
 	fmt.Printf("c1=%c c2=%c\n", c1, c2)
 
 	//输出中文字符可以用int rune存放
-	var c3 rune = '北'
-	fmt.Printf("c3=%c", c3)
+	var c3 int = '北'
+	fmt.Printf("c3=%c\n", c3)
+
+	/*
+		Go语言中字符使用utf-8编码
+		其中英文占一个字节
+		汉字占三个字节
+	*/
+	fmt.Printf("c1占%d个字节 c3占%d个字节", unsafe.Sizeof(c1), unsafe.Sizeof(c3))
+
 }
