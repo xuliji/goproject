cf90d50beb917d060d36a831e9496e3827806009
xuliji <2384465976@qq.com>
2022-11-06 15:04:49 +0800

    update

---
+++ src/go_code/chapter05/demo01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+顺序控制流程:按代码顺序执行
+分支控制流程: if-else
+循环控制流程:
+*/
+func main() {
+	fmt.Println("第五章 流程控制\n顺序控制\n分支控制\n循环控制")
+	var age byte
+	fmt.Println("请输入你的年龄", &age)
+	fmt.Scanln(&age)
+	if age >= 18 {
+		fmt.Println("你已经成年了,需要对自己的行为负责.")
+	}
+}
