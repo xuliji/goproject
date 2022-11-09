43c69d0c37b65f7a98f270a06df7acdf8d1552c9
xuliji <2384465976@qq.com>
2022-11-09 19:23:15 +0800

    update

--- src/go_code/chapter05/fordemo/demo02.go
+++ src/go_code/chapter05/fordemo/demo02.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+//	for 循环变量初始化；循环条件，循环变量迭代{
+//	    循环语句操作
+//	}
+func main() {
+	// for循环的第二种写法
+	i := 0
+	for i <= 10 {
+		fmt.Printf("j=%v\n", i)
+		i++
+	}
+	fmt.Println(i)
+}
