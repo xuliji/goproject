2b950764ea2ccf45ec5cd68f5844e48f3811e734
xuliji <2384465976@qq.com>
2022-11-15 21:06:56 +0800

    update

--- src/go_code/chapter06/funcdemo02/test.go
+++ src/go_code/chapter06/funcdemo02/test.go
@@ -8,8 +8,16 @@ func test(n1 int) {
 	fmt.Println("test() n1=", n1)
 }
 
+// getSum
+func getSum(n1 int, n2 int) int {
+	return n1 + n2
+}
+
 func main() {
 	n1 := 10
 	test(n1)
 	fmt.Println("main() n1=", n1)
+	n2 := 30
+	sum := getSum(n1, n2)
+	fmt.Println(sum)
 }
