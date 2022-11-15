d9a11cd9af0a682dc072f2c9b8040c4fcd9d6552
xuliji <2384465976@qq.com>
2022-11-15 19:47:28 +0800

    update

--- src/go_code/chapter06/fundemo01/main/main.go
+++ src/go_code/chapter06/fundemo01/main/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"go_code/chapter06/fundemo01/utils"
+	util "go_code/chapter06/fundemo01/utils" // 包可以取别名，但是原先的名字不能用了
 )
 
 func main() {
 	var n1, n2 = 20.0, 30.0
 	var operator byte = '*'
 
-	res := utils.Cal(n1, n2, operator)
+	res := util.Cal(n1, n2, operator)
 	fmt.Println("计算结果是：", res)
 }
