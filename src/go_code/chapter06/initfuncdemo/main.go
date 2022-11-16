1a70575b74a4f5bede79787331ae7afc62db2c76
xuliji <2384465976@qq.com>
2022-11-16 15:06:04 +0800

    update

--- src/go_code/chapter06/initfuncdemo/main.go
+++ src/go_code/chapter06/initfuncdemo/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goproject/src/go_code/chapter06/initfuncdemo/utils"
+)
 
 // 源文件中先执行全局变量定义，再执行init()函数，再执行main()函数
 var age = test()
 
 func test() int {
 	fmt.Println("test()....")
-	return 90
+	return utils.Age
 }
 
 // init函数在main()函数之前被执行，用于初始化操作
@@ -18,4 +21,5 @@ func init() {
 func main() {
 	fmt.Println("main()....")
 	fmt.Println(age)
+	fmt.Println(utils.Name)
 }
---
+++ src/go_code/chapter06/initfuncdemo/utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+var Age int
+var Name string
+
+func init() {
+	Age = 100
+	Name = "peter"
+}
