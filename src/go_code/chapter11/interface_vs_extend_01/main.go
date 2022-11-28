486fec8f40f39868417ed8680009af8e4886e240
xuliji <2384465976@qq.com>
2022-11-28 13:49:45 +0800

    update

--- src/go_code/chapter11/interface_vs_extend_01/main.go
+++ src/go_code/chapter11/interface_vs_extend_01/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "goproject/src/go_code/chapter11/interface_vs_extend/classes"
+import "goproject/src/go_code/chapter11/interface_vs_extend_01/classes"
 
 func main() {
 	monkey := classes.YoungMonkey{classes.OldMonkey{Name: "悟空"}}
