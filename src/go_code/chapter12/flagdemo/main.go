b031418babf78d6e1c5f04cc8732dff35c40e827
xuliji <2384465976@qq.com>
2022-11-29 20:43:36 +0800

    update

--- src/go_code/chapter12/flagdemo/main.go
+++ src/go_code/chapter12/flagdemo/main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 // flag包来解析命令行参数
 func main() {
+	// 定义变量用于接收命令行参数值
+	var user, pwd, host string
+	var port int
 
+	// 接收用户名
+	flag.StringVar(&user, "u", "root", "用户名, 默认为root")
+	flag.StringVar(&pwd, "pwd", "123456", "密码, 默认为123456")
+	flag.StringVar(&host, "h", "192.168.3.1", "主机, 默认为192.168.3.1")
+	flag.IntVar(&port, "p", 8080, "端口号,默认为8080")
+
+	fmt.Println(user, pwd, host, port)
 }
